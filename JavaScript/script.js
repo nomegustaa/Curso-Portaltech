f36e215975498a40ae751d6a65c151344a9d525e
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2022-10-29 00:09:10 -0300

    ExercicioLaçoRepetição

---
+++ JavaScript/script.js
@@ -0,0 +1,20 @@
+// let contador = 1;
+
+
+// while(contador <= 20){
+//         console.log(contador)
+//        contador++
+        
+// }
+
+
+let contador1 = 20
+
+while(contador1 >= 0){
+    console.log(`andar de número ${contador1}`)
+    contador1 = contador1 - 1
+    if(contador1 === 13){
+        contador1 = contador1 - 1
+    }
+    
+}
