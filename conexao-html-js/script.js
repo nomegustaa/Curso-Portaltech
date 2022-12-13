50ff48af0bda231aa53177d6b39708b8912805b3
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2022-12-13 20:07:22 -0300

    conexao-html-js

---
+++ conexao-html-js/script.js
@@ -0,0 +1,13 @@
+console.log('Bem-vindo ao meu site')
+
+
+const textH3 = document.querySelector('[data-text-h3]')
+const textH2 = document.getElementById('text-h3')
+
+
+textH3.innerHTML = '<h6>Segue em todas nossas redes sociais<h6>'
+
+
+textH2.innerText = 'Testando...'
+
+alert('alert testando')
